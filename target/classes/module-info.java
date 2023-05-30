c04c695ff280dd9b0952b9dc01859a58e02766da
tugalsan karabacak <tugalsan@gmail.com>
2023-05-30 15:21:12 +0300

    Initial commit

---
+++ target/classes/module-info.java
@@ -0,0 +1,6 @@
+module com.tugalsan.api.file.pom {
+    requires java.xml;
+    requires com.tugalsan.api.log;
+    requires com.tugalsan.api.file.xml;
+    exports com.tugalsan.api.file.pom.server;
+}
