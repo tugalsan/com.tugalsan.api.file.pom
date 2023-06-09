cfb55ab45030738cf5414e6ab2a4c736f4702244
tugalsan karabacak <tugalsan@gmail.com>
2023-06-09 17:18:02 +0300

    .

---
+++ target/classes/com/tugalsan/api/file/pom/server/TS_FilePomConfig.java
@@ -0,0 +1,32 @@
+package com.tugalsan.api.file.pom.server;
+
+import com.tugalsan.api.list.client.TGS_ListUtils;
+import java.nio.file.Path;
+import java.util.List;
+
+public class TS_FilePomConfig {
+
+    private TS_FilePomConfig(Path mavenCodes, Path mavenRepository, Path mavenRepositoryProjects, List<String> projectGroupIds) {
+        this.mavenCodes = mavenCodes;
+        this.mavenRepository = mavenRepository;
+        this.mavenRepositoryProjects = mavenRepositoryProjects;
+        this.projectGroupIds = projectGroupIds;
+    }
+    public Path mavenCodes;
+    public Path mavenRepository;
+    public Path mavenRepositoryProjects;
+    public List<String> projectGroupIds;
+
+    public static TS_FilePomConfig of(Path mavenCodes, Path mavenRepository, Path mavenRepositoryProjects, String... projectGroupIds) {
+        return new TS_FilePomConfig(mavenCodes, mavenRepository, mavenRepositoryProjects, TGS_ListUtils.of(projectGroupIds));
+    }
+
+    public static TS_FilePomConfig of() {
+        return of(
+                Path.of("C:\\me\\codes"),
+                Path.of("C:\\Users\\tugal\\.m2\\repository"),
+                Path.of("C:\\Users\\tugal\\.m2\\repository").resolve("com").resolve("tugalsan"),
+                "com.tugalsan"
+        );
+    }
+}
